a7fdd5970c8dd923e5680eb9542e955579fbfbf7
sotojn <jsoto@terascope.com>
2025-04-11 12:04:20 -0700

    add base asset file structure

---
+++ README.md
@@ -0,0 +1,28 @@
+# chaos-assets
+
+> Teraslice standard processor asset bundle
+
+## Getting Started
+
+This asset bundle requires a running Teraslice cluster, you can find the documentation [here](https://github.com/terascope/teraslice/blob/master/README.md).
+
+```bash
+# Step 1: make sure you have teraslice-cli installed
+yarn global add teraslice-cli
+
+# Step 2:
+teraslice-cli assets deploy <cluster-alias> --build
+
+```
+
+## Operations
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](./LICENSE) licensed.
---
+++ jest.config.js
@@ -0,0 +1,44 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+    verbose: true,
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['jest-extended/all'],
+    collectCoverage: true,
+    coverageReporters: ['json', 'lcov', 'text', 'html'],
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        '<rootDir>/asset/**/*.ts',
+        '!<rootDir>/asset/src/index.ts',
+        '!<rootDir>/packages/*/**/*.ts',
+        '!<rootDir>/packages/*/test/**',
+        '!<rootDir>/**/coverage/**',
+        '!<rootDir>/**/*.d.ts',
+        '!<rootDir>/**/dist/**',
+        '!<rootDir>/**/coverage/**'
+    ],
+    testMatch: [
+        '<rootDir>/test/**/*-spec.{ts,js}',
+        '<rootDir>/test/*-spec.{ts,js}'
+    ],
+    moduleNameMapper: {},
+    preset: 'ts-jest',
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+        ignoreDirectories: ['dist'],
+        availableExtensions: ['.js', '.ts', '.mjs']
+    },
+    transform: {
+        '\\.[jt]sx?$': ['ts-jest',
+            {
+                isolatedModules: true,
+                useESM: true
+            }]
+    },
+    setupFiles: [
+        '<rootDir>/test/test.setup.js'
+    ]
+};
