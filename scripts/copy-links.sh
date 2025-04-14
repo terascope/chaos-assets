8ecd497c314c2a94a98b02ca913a2fd43d4b22ba
sotojn <jsoto@terascope.com>
2025-04-14 09:39:49 -0700

    remove un-needed links in copy-links.sh file

--- scripts/copy-links.sh
+++ scripts/copy-links.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-main() {
-    local dest="/asset/node_modules/@terascope/standard-asset-apis"
-    if [ -d "$dest" ]; then
-        echo "* copying the files from standard-asset-apis"
-        rm "$dest"
-        cp -R ./packages/standard-asset-apis/* "$dest"
-    fi
-}
+main() {}
 
 main "$@"
